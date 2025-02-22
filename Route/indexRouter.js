166be1141b6ffd15366a025699b7eff710de9148
magedo992 <ahmeaged900@gmail.com>
2025-02-22 22:56:08 +0200

    add animal3

--- Route/indexRouter.js
+++ Route/indexRouter.js
@@ -1,11 +1,13 @@
 const authRoute=require('./authRoute');
 const PlantRouter=require('./plantRoute');
 const Veterinary=require('./animalCaseRoute');
+const animal=require('./animalRoute')
 
 const mountRouter=(app)=>{
     app.use('/api',authRoute);
     app.use('/plant',PlantRouter);
     app.use('/Veterinary',Veterinary);
+    app.use('/animal',animal)
 
 }
 
